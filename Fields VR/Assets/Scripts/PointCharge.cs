c05a76478bf72c0a8bb9f09e193d33c7868b095f
ssalanski <stefan.salanski@gmail.com>
2019-03-09 19:48:15 -0500

    move PointCharge color change out of the Update method

--- Fields VR/Assets/Scripts/PointCharge.cs
+++ Fields VR/Assets/Scripts/PointCharge.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointCharge : MonoBehaviour {
+public class PointCharge : MonoBehaviour
+{
 
-    public float charge;
+    public float charge;   
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        UpdateColor();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    void UpdateColor()
+    {
         float h = charge > 0 ? 0 : 0.66f;
         float x = 1 - 1 / (1 + Mathf.Abs(charge));
         float s = Mathf.Sqrt(x);
         float v = 0.2f + 0.8f * x;
         GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(h, s, v);
-	}
+    }
 }
