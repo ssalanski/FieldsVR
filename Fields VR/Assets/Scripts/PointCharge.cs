829b46ad303f2f2b8ed94d633b129fad9cbd68f9
ssalanski <stefan.salanski@gmail.com>
2019-03-09 19:09:25 -0500

    change color of particle sphere based on charge

--- Fields VR/Assets/Scripts/PointCharge.cs
+++ Fields VR/Assets/Scripts/PointCharge.cs
@@ -13,6 +13,10 @@ public class PointCharge : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float h = charge > 0 ? 0 : 0.66f;
+        float x = 1 - 1 / (1 + Mathf.Abs(charge));
+        float s = Mathf.Sqrt(x);
+        float v = 0.2f + 0.8f * x;
+        GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(h, s, v);
 	}
 }
