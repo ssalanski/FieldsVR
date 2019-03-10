3b9ac5002fde914580df6f3515ad654789a83bca
ssalanski <stefan.salanski@gmail.com>
2019-03-09 19:29:43 -0500

    renaming to ElectricFieldArray in preparation for handling a 3d grid of vectors

--- Fields VR/Assets/Scripts/ElectricFieldArray.cs
+++ Fields VR/Assets/Scripts/ElectricFieldArray.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElectricField : MonoBehaviour
+public class ElectricFieldArray : MonoBehaviour
 {
     public GameObject arrowPrefab;
 
